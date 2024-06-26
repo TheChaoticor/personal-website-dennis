50663b19c194875f220a24abfe13246783445bc5
Arpit Mishra <arpitbbsrboy@gmail.com>
2024-06-26 20:46:30 +0530

    added contact page along with routing

--- mywebsite/base/urls.py
+++ mywebsite/base/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('team/', views.team, name='team'),
     path('project/', views.project, name='project'),
     path('calendar/', views.calender, name='calender'),
+    path('team/', views.get_started, name='get_started'),
+    path('contact/', views.contact, name='contact'),
+    path('success/',views.success,name='success'),
+    
 ]
--- mywebsite/base/views.py
+++ mywebsite/base/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .forms import ContactForm
 
 # Create your views here.
 
@@ -13,5 +14,29 @@ def team(request):
 
 def calender(request):
 	return render(request, 'base/calender.html')
-def basss(request):
-	return render(request, 'base/basss.html')
+
+
+def get_started(request):
+    return render(request, 'base/team.html')
+
+def success(request):
+    return render (request,'base/success.html')
+
+def contact(request):
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            # Process the form data
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+            
+            # Here you can do further processing like sending emails, saving to database, etc.
+            # For now, let's redirect to a success page
+            return redirect('success')  # Assuming 'success' is the name of your success URL pattern
+    else:
+        form = ContactForm()
+    
+    return render(request, 'base/contact.html', {'form': form})
+
