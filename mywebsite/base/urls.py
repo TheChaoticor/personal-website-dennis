22012749e03ac1a3525c6aa43887da94ac718270
Arpit Mishra <arpitbbsrboy@gmail.com>
2024-06-26 15:21:51 +0530

    url issue resolved

--- mywebsite/base/urls.py
+++ mywebsite/base/urls.py
@@ -1,12 +1,9 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
-	path('', views.home),
-    path('team/',views.team),
-    path('project/',views.project),
-    path('calender/',views.calender),
-    path('basss/',views.basss),
-    
+    path('', views.home, name='dashboard'),
+    path('team/', views.team, name='team'),
+    path('project/', views.project, name='project'),
+    path('calendar/', views.calender, name='calender'),
 ]
